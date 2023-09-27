72edd17ecf507b7e64058505e7ad312d996ed1fa
laura-farias43 <laura.domingues@germinare.org.br>
2023-09-27 10:29:41 -0300

    deletei o componente MenuAdm

--- src/Components/CardPastel/index.js
+++ src/Components/CardPastel/index.js
@@ -1,16 +1,23 @@
-import './styles.css'
+import "./styles.css";
+import BotaoAdd from "../../imagens/botaoAdd.png";
+import pastelCard from "../../imagens/pastelCard.png";
 
-function CardPastel(){
-
-    return(
-
-        <div>
-
-            
+function CardPastel() {
+  return (
+    <div className="container">
+      <div className="background">
+        <h5>Pastel de Carne</h5>
+        <div className="imgdiv">
+          <img id="imgpastel" src={pastelCard} alt="" />
+          <p id="preco">R$10,90</p>
         </div>
-
-    )
+      </div>
+      <div className="buttons">
+        <button className="detalhes">Ver detalhes</button>
+        <button id="mais">+</button>
+      </div>
+    </div>
+  );
 }
-
 
 export default CardPastel;
